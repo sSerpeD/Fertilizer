11877f7156ba284c40f83b6aadcb94acd1515f1f
Thanapat tardtong <fertilizerweb@parlab.cs.tu.ac.th>
2023-04-02 17:37:35 +0700

    update

--- database.js
+++ database.js
@@ -1,11 +1,22 @@
-const mysql = require("mysql2");
+const mysql = require("mysql");
 
 const dbConnection = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "root",
-    database: "nodejs_login",
-    port: "3306"
-}).promise()
+    password: "Th4n4p4t$",
+    connectionLimit: 10
+})
+
+dbConnection.query(`INSERT INTO WebFertilizer.users (card_id, password) VALUES ('1102800113194', '123456')`)
+dbConnection.query(`SELECT * FROM WebFertilizer.users`, (err, res) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(res);
+    }
+});
+
+
 
 module.exports = dbConnection;
+
